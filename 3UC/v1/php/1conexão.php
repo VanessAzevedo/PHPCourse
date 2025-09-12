5de61f1d4821dce991a33b29ef8e1eb5aa8e3887
Programação PHP 36129522025.3 <36129522025.3@senacrjedu.local>
2025-09-12 09:15:37 -0300

    3UC

---
+++ 3UC/v1/php/1conexão.php
@@ -0,0 +1,11 @@
+<?php
+$usuario = $_POST['nome'];
+$senha = $_POST['senha'];
+
+//CONEXÃO COM O BANCO:
+$servidor = 'localhost';
+$user_banco = 'root';
+$senha_banco = '';
+$banco = 'projetoloja';
+
+$conexão = new mysqli($servidor, $user_banco, $senha_banco, $banco);
---
+++ 3UC/v1/php/verificaLogin.php
@@ -0,0 +1,33 @@
+<?php
+// Iniciando a sessão
+session_start();
+
+// Conexão com o banco
+require('conexao.php'); // "vai requerer" o arquivo conexao.php
+
+// Os dados abaixo vieram do form html
+$usuario = $_POST['nome'];
+$senha = $_POST['senha'];
+
+$_SESSION['nome'] = $usuario;
+
+
+// procurando no banco os dados
+$sql = "SELECT * FROM login WHERE usuario = '$usuario' AND senha = '$senha' ";
+$procurar = $conexao->query($sql); // ele executa o "select" de $sql
+
+// verificando se existe
+if ($procurar->num_rows > 0){
+    // caso o usuário esteja correto
+    // Ele vai redirecionar para a página 'admin.html'
+
+} else {
+    // caso o usuário não exista
+    // Ele vai informar que o usuário não existe
+    echo "Usuário e Senha incorretos ";
+    echo "<a href='../index.html'>Voltar</a>";
+}
+
+
+
+?>
